e9daae0241e37be1c591b5d06434b7660e8b58d1
AniketHalder <87072339+AniketHalder@users.noreply.github.com>
2023-06-27 18:24:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+img = "";
+status = "";
+objects = "";
+alarm = "";
+
+function setup()
+{
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();   
+}
+
+function preload()
+{
+    alarm= loadSound('alarm.mp3');
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log('modelLoaded');
+    status = true;
+}
+
+function draw()
+{
+    image(video, 0, 0, 380, 380);
+    
+    if(status != "")
+    {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+
+        objectDetector.detect(video, gotResults);
+        for(i=0; i < objects.length; i++ )
+        {
+            document.getElementById("status").innerHTML = "Status : Baby Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects Detected are : " + objects.length;
+            
+            fill(r,g,b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label+""+ percent+"%", objects[i].x +15 , objects[i].y+15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+    else
+    {
+        document.getElementById("status").innerHTML = "Status : Baby not Detected";
+    }
+}
+
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
